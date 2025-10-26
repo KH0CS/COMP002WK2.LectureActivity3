5a20f4d33d684b1f9ff87c27d1ab07fe89ee3acb
Kam Rodriguez <kamrod1021@gmail.com>
2025-10-26 13:17:34 -0700

    I added the correct switch statements to the JS

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
+/*
 let myGrade = 88; 
 
 if (myGrade >= 90) {
-    console.log("You got an A");
+    console.log ("You got an A");
 } 
-else if (myGrade < 89 && myGrade > 80) {
-    console.log("You got a B");
+else if (myGrade <= 89 && myGrade >= 80) {
+    console.log ("You got a B");
 }
-else if (myGrade < 79 && myGrade >= 70) {
-    console.log("You got a C");
+else if (myGrade <= 79 && myGrade >= 70) {
+    console.log ("You got a C");
 }
-else if (myGrade < 69 && myGrade >= 60) {
-    console.log("You got a D");
+else if (myGrade <= 69 && myGrade >= 60) {
+    console.log ("You got a D");
 }
-else if (myGrade < 59) {
-    console.log("You got an F!");
+else if (myGrade <= 59) {
+    console.log ("You got an F!");
 }
 // I believe the last one you're gonna do would be your else statement. I think because there is no in between from 59 everything 59 and below is an F.
+*/
+let Grade = "B";
 
-let MyGrade = 74;
+switch (Grade) {
+    case ("A"):
+        console.log ("You got an A -Switch Statement");
+        break;
+    case ("B"):
+        console.log ("You got a B -Switch Statement");
+        break;
+    case ("C"):
+        console.log ("You got a C -Switch Statement");
+        break;
+    case ("D"):
+        console.log ("You got a D -Switch Statement");
+        break;
+    case ("F"):
+        console.log ("You got an F -Switch Statement");
 
-switch (MyGrage) {
-    case (MyGrade >= 90):
-        console.log("You got an A");
-        break;
-    case (MyGrade < 89 && MyGrade > 80):
-        console.log("You got a B");
-        break;
-    
 }
