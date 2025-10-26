88cae61f9c959062959e1c3bda50cd4bb1e9fa1d
Kam Rodriguez <kamrod1021@gmail.com>
2025-10-26 09:51:17 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let myGrade = 88; 
+
+if (myGrade >= 90) {
+    console.log("You got an A");
+} 
+else if (myGrade < 89 && myGrade > 80) {
+    console.log("You got a B");
+}
+else if (myGrade < 79 && myGrade >= 70) {
+    console.log("You got a C");
+}
+else if (myGrade < 69 && myGrade >= 60) {
+    console.log("You got a D");
+}
+else if (myGrade < 59) {
+    console.log("You got an F!")
+}
