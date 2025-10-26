a2542c989301c36ed790772a3429a801bc643e01
Kam Rodriguez <kamrod1021@gmail.com>
2025-10-26 12:49:59 -0700

    I added some "Switch statements A & B

--- script.js
+++ script.js
@@ -13,5 +13,18 @@ else if (myGrade < 69 && myGrade >= 60) {
     console.log("You got a D");
 }
 else if (myGrade < 59) {
-    console.log("You got an F!")
+    console.log("You got an F!");
+}
+// I believe the last one you're gonna do would be your else statement. I think because there is no in between from 59 everything 59 and below is an F.
+
+let MyGrade = 74;
+
+switch (MyGrage) {
+    case (MyGrade >= 90):
+        console.log("You got an A");
+        break;
+    case (MyGrade < 89 && MyGrade > 80):
+        console.log("You got a B");
+        break;
+    
 }
